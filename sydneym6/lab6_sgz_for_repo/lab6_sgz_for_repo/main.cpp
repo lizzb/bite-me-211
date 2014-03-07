a7ad42f90d984b4dce8852524ebd95dac3c4e005
Sydney Zink <sydney.zink@gmail.com>
2014-03-06 21:54:07 -0600

    added Xcode file

---
+++ sydneym6/lab6_sgz_for_repo/lab6_sgz_for_repo/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  lab6 sgz for repo
+//
+//  Created by Sydney Zink on 3/6/14.
+//  Copyright (c) 2014 Sydney Zink. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
